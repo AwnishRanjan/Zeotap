e031fc787706c26747d88340dc2a6cdb4f12f2bf
Awnish Ranjan <awnishranjan@Awnishs-MacBook-Air.local>
2024-10-23 21:54:13 +0530

    final commit

--- src/rule_modifier.py
+++
@@ -1,5 +0,0 @@
-def modify_operator(ast, new_operator):
-    if ast.type == 'operator':
-        ast.value = new_operator
-    else:
-        raise ValueError("Can't modify operator of an operand node.")
